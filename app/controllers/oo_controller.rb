baf3c8b8f43337ab4367c49ab5748ca6026db72d
Xiaoyong Cheng <tinaxycheng@gmail.com>
2015-12-06 10:11:38 -0500

    Tested out the website sturcture, with blog, about and slides

---
+++ app/controllers/oo_controller.rb
@@ -0,0 +1,27 @@
+class OoController < ApplicationController
+  def home
+  end
+  def new
+  end
+  def show
+  	@category = params
+  	if @category = 1
+  		render :blog
+  	elsif @category = 2
+  		render :slides
+  	elsif @category = 3
+  		render :register
+
+  	end
+  end
+  def blog
+  end
+  def slides
+  end
+  def register
+  end
+  def create
+  	@user = User.new(create_params)
+  end
+
+end
---
+++ app/helpers/oo_helper.rb
@@ -0,0 +1,2 @@
+module OoHelper
+end
