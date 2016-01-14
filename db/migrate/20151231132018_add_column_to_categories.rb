4b677bb4cc22237b42ecf979b924dc6cd26f2bf0
Xiaoyong Cheng <tinaxycheng@gmail.com>
2016-01-14 00:13:38 -0500

    migration

--- db/migrate/20151231132018_add_column_to_categories.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToCategories < ActiveRecord::Migration
-  def change
-    add_column :categories, :name, :string
-    add_reference :categories, :blog, index: true, foreign_key: true
-  end
-end
