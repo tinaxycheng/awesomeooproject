bd8427e2a67ac6cbb663f31dd3b5e6bdb8e42ac2
Xiaoyong Cheng <tinaxycheng@gmail.com>
2016-01-19 23:21:55 -0500

    17

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,5 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],
     :region => 'us-west-2'                        # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]                     # required
+  config.fog_directory  = ENV["AWS_BUCKET"]                      # required
 end
+require 'excon'
+Excon.ssl_verify_peer = false
