fbe1c8f04e599ce81503f283422dabd0b7397ad2
Xiaoyong Cheng <tinaxycheng@gmail.com>
2018-10-10 09:30:01 -0400

    fix admin error

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   get 'projects/show'
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", :registrations => "registrations" }
   root 'oo#home'
   resources :oo
