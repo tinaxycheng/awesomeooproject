dfa01f65de307f1136719ac817afb5139886bb4f
Xiaoyong Cheng <tinaxycheng@gmail.com>
2021-08-20 14:17:59 -0400

    image upload settings for heroku

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -12,8 +12,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
   end
   include CarrierWave::MiniMagick
   process resize_to_fit: [300, 300]
