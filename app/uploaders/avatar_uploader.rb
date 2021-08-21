c719197fdfabcf2ee5c5ba8be6aa53d0102821d8
Xiaoyong Cheng <tinaxycheng@gmail.com>
2021-08-20 23:56:13 -0400

    upload cache added

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,6 +15,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  def cache_dir
+     "#{Rails.root}/tmp/uploads"
+  end
   include CarrierWave::MiniMagick
   process resize_to_fit: [300, 300]
 
